fa08f2327d3e01fa4db62ad2700f4a7e1297e3c2
Jon Nehring <stonetip>
2014-12-11 11:10:39 -0700

    Added comments

--- RealtimeRun/MapHub.cs
+++ RealtimeRun/MapHub.cs
@@ -4,8 +4,11 @@ namespace RealtimeRun
 {
     public class MapHub : Hub
     {
+        // The Send method is used on a client to deliver data to the server
         public void Send(string lat, string lon, double? altitude, double? speed)
         {
+            // In return, the server broadcasts the data to all clients. 
+            // Those that have the broadcastLatLon method will use the data on the client
             Clients.All.broadcastLatLon(lat, lon, altitude, speed);
         }
     }
